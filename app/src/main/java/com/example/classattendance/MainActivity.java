f17ec040e7399ae67ed85f13e41f21ec681713e0
Dinh201 <92248338+Dinh201@users.noreply.github.com>
2024-04-28 16:37:17 +0700

    Add files via upload

--- app/src/main/java/com/example/classattendance/MainActivity.java
+++ app/src/main/java/com/example/classattendance/MainActivity.java
@@ -1,98 +1,91 @@
-package com.example.classattendance;
+package com.example.classattendance.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.example.classattendance.R;
 import com.example.classattendance.databinding.ActivityMainBinding;
-import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.example.classattendance.fragment.FirstFragment;
+import com.example.classattendance.utils.MyAuth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-    DrawerLayout drawLayout;
-    ImageButton buttonDrawerToggle;
-    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-        drawLayout = findViewById(R.id.drawerLayout);
-        buttonDrawerToggle = findViewById(R.id.buttonDrawerToggle);
-        navigationView = findViewById(R.id.nav_view);
-
-        buttonDrawerToggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawLayout.open();
-            }
-        });
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                int itemId = menuItem.getItemId();
-
-                if(itemId == R.id.nav_school){
-                    Toast.makeText(MainActivity.this, "School Clicked", Toast.LENGTH_SHORT).show();
-                }
-                if(itemId == R.id.nav_calendar){
-                    Toast.makeText(MainActivity.this, "Calendar Clicked", Toast.LENGTH_SHORT).show();
-                }
-                if(itemId == R.id.nav_Class1){
-                    Toast.makeText(MainActivity.this, "Class1 Clicked", Toast.LENGTH_SHORT).show();
-                }
-                if(itemId == R.id.nav_Class2){
-                    Toast.makeText(MainActivity.this, "Class2 Clicked", Toast.LENGTH_SHORT).show();
-                }
-                if(itemId == R.id.nav_Class3){
-                    Toast.makeText(MainActivity.this, "Class3 Clicked", Toast.LENGTH_SHORT).show();
-                }
-                if(itemId == R.id.nav_class_passed){
-                    Toast.makeText(MainActivity.this, "Class-passed Clicked", Toast.LENGTH_SHORT).show();
-                }
-                if(itemId == R.id.nav_folder){
-                    Toast.makeText(MainActivity.this, "Folder Clicked", Toast.LENGTH_SHORT).show();
-                }
-                if(itemId == R.id.nav_setting){
-                    Toast.makeText(MainActivity.this, "Setting Clicked", Toast.LENGTH_SHORT).show();
-                }
-                if(itemId == R.id.nav_help){
-                    Toast.makeText(MainActivity.this, "Help Clicked", Toast.LENGTH_SHORT).show();
-                }
-
-                drawLayout.close();
-
-                return false;
-            }
-        });
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
 
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        appBarConfiguration =
+                new AppBarConfiguration
+                        .Builder(navController.getGraph())
+                        .setOpenableLayout(drawer)
+                        .build();
+
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
+        navigationView.setNavigationItemSelectedListener(item -> {
+            Fragment fragment = null;
+            int id = item.getItemId();
+            if (id == R.id.nav_school) {
+                fragment = new FirstFragment();
+                getIntent().putExtra("filter", "all");
+            } else if (id == R.id.nav_created_classes) {
+                fragment = new FirstFragment();
+                getIntent().putExtra("filter", "created_only");
+            } else if (id == R.id.nav_joined_classes) {
+                fragment = new FirstFragment();
+                getIntent().putExtra("filter", "joined_only");
+//            } else if (id == R.id.nav_notification) {
+//                fragment = new FragmentNotification();
+//            } else if (id == R.id.nav_attendance) {
+//                fragment = new FragmentAttendance();
+            } else if (id == R.id.nav_signout) {
+                MyAuth.signOut();
+                // Đăng xuất Google SignIn Client nếu bạn sử dụng
+                GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN);
+                googleSignInClient.signOut().addOnCompleteListener(this,
+                        task -> {
+                            // Intent mới với cờ CLEAR_TOP để làm sạch ngăn xếp hoạt động
+                            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish(); // Gọi finish() để kết thúc MainActivity này
+                        });
+            }
 
-
+            if (fragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_main, fragment).commit(); // Use the correct container ID
+            }
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        });
     }
 
     @Override
