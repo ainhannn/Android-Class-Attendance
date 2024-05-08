262468a6c849dc54def00a974ddbac52030dd2ca
Calcifer <ainhan610@gmail.com>
2024-05-08 12:22:43 +0700

    remove gitignore

--- app/src/main/java/com/example/classattendance/activity/LoginActivity.java
+++ app/src/main/java/com/example/classattendance/activity/LoginActivity.java
@@ -67,7 +67,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void navigateToMain() {
-        setModelUserAfterLogin();
+        // setModelUserAfterLogin();
 
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
